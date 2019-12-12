0d94e065c8781749fab9cb32f02e3422159e3d8b
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-12-12 09:41:52 +0900

    Feed 모델 생성

---
+++ EssentialFedd/EssentialFedd.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFedd.h
+//  EssentialFedd
+//
+//  Created by zombietux on 2019/12/12.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFedd.
+FOUNDATION_EXPORT double EssentialFeddVersionNumber;
+
+//! Project version string for EssentialFedd.
+FOUNDATION_EXPORT const unsigned char EssentialFeddVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFedd/PublicHeader.h>
+
+
---
+++ EssentialFedd/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFedd
+//
+//  Created by zombietux on 2019/12/12.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id : UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
